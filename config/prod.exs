8106a4c554b5b38a2f7c8b122d8dda4cdb247592
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-07-01 15:25:32 -0400

    Add release configs

--- config/prod.exs
+++ config/prod.exs
@@ -54,12 +54,8 @@ config :logger, level: :info
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
-#
-#     config :parking, ParkingWeb.Endpoint, server: true
-#
+
+config :parking, ParkingWeb.Endpoint, server: true
+
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-import_config "prod.secret.exs"
