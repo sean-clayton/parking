701b24f83874b501497f050b798619198bea52a1
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-07-01 15:39:45 -0400

    Add libcluster

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Parking.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:libcluster, "~> 3.1.0"}
     ]
   end
 end
